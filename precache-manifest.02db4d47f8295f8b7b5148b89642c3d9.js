239b34e34320598793ca1a277994424a9516c65f
Jeankennethsc02 <staclarajeankeenth@gmail.com>
2020-02-10 20:36:32 +0800

    Updates

---
+++ precache-manifest.02db4d47f8295f8b7b5148b89642c3d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81fcf437327d033548ec5b71bd174fed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2808073d3a1acbe7628",
+    "url": "/static/css/main.4b5c3934.chunk.css"
+  },
+  {
+    "revision": "b764edeb2e2db3b0f9d8",
+    "url": "/static/js/2.7802d5cf.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.7802d5cf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a2808073d3a1acbe7628",
+    "url": "/static/js/main.01799766.chunk.js"
+  },
+  {
+    "revision": "21e75a0f42b280cf2cd7",
+    "url": "/static/js/runtime-main.6b1fa9e0.js"
+  }
+]);
